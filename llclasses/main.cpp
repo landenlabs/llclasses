ce3d4ce773aacb23e33a1436d253221c4d29933b
landenlabs2 <landenlabs2@gmail.com>
2019-01-31 21:49:35 -0500

    Initial Commit

---
+++ llclasses/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  llclasses
+//
+//  Created by Dennis Lang on 1/31/19.
+//  Copyright © 2019 Dennis Lang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
