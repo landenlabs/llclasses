3775123046f09ac3efc20c6f3f4c13317098acb9
landenlabs2 <landenlabs2@gmail.com>
2020-01-23 08:54:54 -0500

    update readme

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
 
 # llclasses
-OSX / Linux / DOS  File find and replace text command line tool.
+OSX / Linux / DOS  Object Oriented Code parser
 
 Parse C++ or Java files and output class hierarchy relationship. 
 
   [![Build status](https://travis-ci.org/landenlabs/llclasses.svg?branch=master)](https://travis-ci.org/landenlabs/llclasses)
+  [![Known Vulnerabilities](https://snyk.io/test/github/landenlabs/llclasses/badge.svg)](https://snyk.io/test/github/landenlabs/llclasses)
+
   
-  
-Help screen
+**Help screen**
 
 <pre>
 
 </pre>
 
 
-Visit home website
+Details on website
+[http://landenlabs.com/code/llclasses/index.html]
 
+Previous unsupported legacy program which only parsed Java
+[http://landenlabs.com/code/javatree/index.html]
+
+Example diagrams of Android classes generated using llclasses
+[http://landenlabs.com/android/classtree/classtree.html]
+
+Home website
 [http://landenlabs.com](http://landenlabs.com)
 
---
+++ samples/test/test.java
@@ -0,0 +1,47 @@
+
+
+private 
+    static 
+    final 
+    class 
+        LoaderReference 
+    extends 
+    WeakReference<ClassLoader> 
+    implements
+     Map.Entry<K,V>,  Map.Entry<K,V>
+    { public class inner {}; };
+
+private static final class CacheKey implements Cloneable { };
+
+final class EntryIterator extends PrivateEntryIterator<Map.Entry<K,V>> { };
+
+public final class Formatter implements Closeable, Flushable { };
+
+static final class Entry<K,V> implements Map.Entry<K,V> { };
+
+public abstract class Calendar implements Serializable, Cloneable, Comparable<Calendar> { };
+
+public abstract class AbstractSet<E> extends AbstractCollection<E> implements Set<E> { };
+
+static class MutablePair<F, S> { };
+public abstract class UnselectableArrayAdapter<E> extends ArrayAdapter<E> { };
+public class Garage<X extends Vehicle> { };
+class BST<X extends Comparable<X>> { };
+class BST<X extends Comparable<X>> implements Map.Entry<K,? extends V> {};
+int totalFuel(List<? extends Vehicle> list) { };
+
+/* block comment */
+/* block
+   2 lines //  */
+//        1         2         3         4
+//234567890123456789012345678901234567890123
+char test1[]= "//test'/* */ \" more " "more" ;  /* foo */ // more
+char test2= '\"'; // foo /* comment */
+bool test3   =    std::string(  "hello"  ) == /* comment */ "world"; // comment
+class foo
+{
+    int bar;
+};
+
+
+done
